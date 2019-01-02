34c4698b53b6cc8081d6d32f82a8e8af20371822
Park Chanjong <coldbell4869@gmail.com>
2019-01-02 12:58:13 +0900

    fixed #1 커밋커밋

--- CalAdd.java
+++ CalAdd.java
@@ -2,13 +2,12 @@ package my.examples.javaexam.algorithm;
 
 import java.util.Scanner;
 
-// 계산기 더하기
 public class CalAdd {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
         int sum = a+b;
-        System.out.println(sum);
+        System.out.println(a+" 와 "+b+" 를 더하면 "+ sum + " 입니다.");
     }
 }
