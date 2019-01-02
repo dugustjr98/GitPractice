e1d3108a48856e5b33b2450f9a344b5defd75cc0
Park Chanjong <coldbell4869@gmail.com>
2019-01-02 12:34:55 +0900

    계산기 더하기 기능

---
+++ CalAdd.java
@@ -0,0 +1,13 @@
+package my.examples.javaexam.algorithm;
+
+import java.util.Scanner;
+
+public class CalAdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a+b;
+        System.out.println(sum);
+    }
+}
